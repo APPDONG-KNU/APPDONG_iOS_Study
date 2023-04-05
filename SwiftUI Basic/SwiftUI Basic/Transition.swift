8ed18ff9917add9f6aa52da01c7a9055f2cd8b04
보경 <97583162+bokoo14@users.noreply.github.com>
2023-04-05 22:57:43 +0900

    feat: transition

--- SwiftUI Basic/SwiftUI Basic/Transition.swift
+++ SwiftUI Basic/SwiftUI Basic/Transition.swift
@@ -15,8 +15,45 @@
 import SwiftUI
 
 struct Transition: View {
+    // property
+    @State var showTransition: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack(alignment: .bottom) {
+            VStack {
+                Button {
+                    withAnimation(.easeOut) {
+                        showTransition.toggle()
+                    }
+                } label: {
+                    Text("Button")
+                }
+                Spacer()
+            }
+            
+            if showTransition {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(height: UIScreen.main.bounds.height * 0.5)
+                    .opacity(showTransition ? 1.0 : 0.0)
+                
+                // 1번 move transition
+                //.transition(.move(edge: .bottom))
+                
+                // 2번 opacity transition
+                // .transition(.opacity)
+                
+                // 3번 scale transition
+                    .transition(.scale)
+                
+                // 4번 asymmetric transition - 시작점 끝점을 custom (비대칭)
+                    .transition(.asymmetric(
+                        insertion: .move(edge: .leading), // 시작하는 위치
+                        removal: .move(edge: .trailing))) // 끝나는 위치
+                
+            }
+            
+        }
+        .ignoresSafeArea(edges: .bottom)
     }
 }
 
