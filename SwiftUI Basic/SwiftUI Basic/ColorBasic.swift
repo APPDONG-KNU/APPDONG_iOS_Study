fb27b0dbb9b7a15b0c6ef66dabe506541590f256
보경 <97583162+bokoo14@users.noreply.github.com>
2023-04-09 16:14:53 +0900

    feat: color

---
+++ SwiftUI Basic/SwiftUI Basic/ColorBasic.swift
@@ -0,0 +1,20 @@
+//
+//  ColorBasic.swift
+//  SwiftUI Basic
+//
+//  Created by Bokyung on 2023/04/09.
+//
+
+import SwiftUI
+
+struct ColorBasic: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ColorBasic_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorBasic()
+    }
+}
