233e63828aad7e6a34ab015cd5823fca7889742b
보경 <97583162+bokoo14@users.noreply.github.com>
2023-04-11 20:21:09 +0900

    Create PickerBasic.swift

---
+++ SwiftUI Basic/SwiftUI Basic/Core2/PickerBasic.swift
@@ -0,0 +1,20 @@
+//
+//  PickerBasic.swift
+//  SwiftUI Basic
+//
+//  Created by Bokyung on 2023/04/10.
+//
+
+import SwiftUI
+
+struct PickerBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PickerBasic_Previews: PreviewProvider {
+    static var previews: some View {
+        PickerBasic()
+    }
+}
