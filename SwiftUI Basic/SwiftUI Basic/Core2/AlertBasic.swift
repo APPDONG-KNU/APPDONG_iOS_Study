ba826f2a2091996e3c7e99524996e5fe3fead594
보경 <97583162+bokoo14@users.noreply.github.com>
2023-04-11 20:21:20 +0900

    Update AlertBasic.swift

--- SwiftUI Basic/SwiftUI Basic/Core2/AlertBasic.swift
+++ SwiftUI Basic/SwiftUI Basic/Core2/AlertBasic.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct AlertBasic: View {
-    // alert:  pop-up 창으로 UI화면에 메세지를 나타냄
+    // alert: pop-up 창으로 UI화면에 메세지를 나타냄
     // property
     @State var showAlert1: Bool = false
     @State var showAlert2: Bool = false
@@ -30,15 +30,35 @@ struct AlertBasic: View {
                      message: Text("정말로 메세지 삭제 하시겠습니까?"),
                      primaryButton: .destructive(Text("delete"), action: {
             backgroundColor = .red
-        }),
-                     secondaryButton:   .cancel())
+        }), secondaryButton: .cancel())
+    }
+    
+    func getAlert3() -> Alert {
+        return Alert(title: Text(alertTitle),
+                     message: Text(alertMessage),
+                     dismissButton: .default(Text("OK")))
+    }
+    
+    func getAlert4() -> Alert {
+        switch alertType {
+        case .error:
+            return Alert(title: Text("에러 발생 다시 확인하세요"), dismissButton: .default(Text("OK"), action: {
+                backgroundColor = .red
+            }))
+        case .success:
+            return Alert(title: Text("로그인에 성공 했습니다"), dismissButton: .default(Text("OK"), action: {
+                backgroundColor = .green
+            }))
+        default:
+            return Alert(title: Text("에러"))
+        }
     }
     
     var body: some View {
         ZStack {
             backgroundColor.ignoresSafeArea()
             
-            VStack {
+            VStack (spacing: 20){
                 // 1번
                 Button {
                     showAlert1.toggle()
@@ -48,7 +68,7 @@ struct AlertBasic: View {
                     Alert(title: Text("패스워드 에러입니다. 다시 확인하세요."))
                 }
                 
-                 // 2번
+                // 2번
                 Button {
                     showAlert2.toggle()
                 } label: {
@@ -56,10 +76,49 @@ struct AlertBasic: View {
                 }.alert(isPresented: $showAlert2) {
                     getAlert2()
                 }
-
-                // 3번
                 
-
+                // 3번
+                HStack (spacing: 10){
+                    Button {
+                        alertTitle = "영상이 업로드 에러"
+                        alertMessage = "영상이 제대로 올라가지 않았습니다. 다시 확인해 주세요"
+                        showAlert3.toggle()
+                    } label: {
+                        Text("3번 Alert 실패 시")
+                    }
+                    
+                    Button {
+                        alertTitle = "영상이 업로드 성공"
+                        alertMessage = "영상이 제대로 올라갔습니다"
+                        showAlert3.toggle()
+                    } label: {
+                        Text("3번 Alert 성공 시")
+                    }
+                } // : HStack
+                .alert(isPresented: $showAlert3) {
+                    getAlert3()
+                }
+                
+                // 4번
+                HStack (spacing: 10){
+                    Button {
+                        alertType = .error
+                        showAlert4.toggle()
+                    } label: {
+                        Text("4번 Alert 실패 시")
+                    }
+                    
+                    Button {
+                        alertType = .success
+                        showAlert4.toggle()
+                    } label: {
+                        Text("4번 Alert 성공 시")
+                    }
+                } // : HStack
+                .alert(isPresented: $showAlert4) {
+                    getAlert4()
+                }
+                
             }
         } // : VStack
     }
