d453fd80c8460e0462fa773435db13301b9368d0
보경 <97583162+bokoo14@users.noreply.github.com>
2023-04-29 16:42:31 +0900

    feat: materialbackground

---
+++ iOS15/iOS15/MaterialBackgroundBasic.swift
@@ -0,0 +1,23 @@
+//
+//  MaterialBackgroundBasic.swift
+//  iOS15
+//
+//  Created by Bokyung on 2023/04/29.
+//
+
+import SwiftUI
+
+struct MaterialBackgroundBasic: View {
+    /**
+     MaterialBackground: 불투명도를 조절하여 특정 색상이 비쳐보이는 효과로 자연스러운 배경 효과를 사용할 수 있음
+     */
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MaterialBackgroundBasic_Previews: PreviewProvider {
+    static var previews: some View {
+        MaterialBackgroundBasic()
+    }
+}
