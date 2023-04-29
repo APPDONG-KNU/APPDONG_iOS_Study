72c3457c777314578c1d242332bbb3a15965beb0
보경 <97583162+bokoo14@users.noreply.github.com>
2023-04-29 19:02:07 +0900

    feat: MaterialBackground

--- iOS15/iOS15/MaterialBackgroundBasic.swift
+++ iOS15/iOS15/MaterialBackgroundBasic.swift
@@ -11,8 +11,40 @@ struct MaterialBackgroundBasic: View {
     /**
      MaterialBackground: 불투명도를 조절하여 특정 색상이 비쳐보이는 효과로 자연스러운 배경 효과를 사용할 수 있음
      */
+    
+    // property
+    let url = URL(string: "https://picsum.photos/1000")
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Spacer()
+            
+            VStack {
+                RoundedRectangle(cornerRadius: 4)
+                    .frame(width: 50, height: 4)
+                    .padding()
+                Spacer()
+                Text("Material Background 예시")
+                    .font(.headline)
+                Spacer()
+            } // :VStack
+            .frame(height: 350)
+            .frame(maxWidth: .infinity)
+            .background(.ultraThinMaterial)
+            /**
+             .thinMaterial
+             .ultraThinMaterial
+             .ultraThickMaterial
+             .thickMaterial
+             .regularMaterial
+             */
+        } // :VStack
+        .background(
+            AsyncImage(url: url) { image in
+                image
+            } placeholder: {
+                ProgressView()
+            })
+        
     }
 }
 
